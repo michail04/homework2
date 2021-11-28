83a572b03b20b2c6040e2b7384a95b587520a4f5
Michail <93787990+michail04@users.noreply.github.com>
2021-11-29 00:48:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import os
 path = os.path.join(os.getcwd(), 'recipes.txt')
 
-with open('recipes.txt') as file:
-    data_name = file['recipes.txt'].encode('utf-8')
-    print(file.read())
-# file= open('recipes.txt')
-# x=file.read()
-# print(x)
-file = open(file, encoding="utf8")
+with open(path) as file:
+    file = open('recipes.txt', encoding="utf8")
+    # print(file.read())
+    result = {}
+    for dish in file:
+        dish_name = dish.strip()
+        count_ingr = int(file.readline().strip())
+        for items in range(count_ingr):
+            structure = []
+            name, qantity, unit = file.readline().split('|')
+            structure.append({'name': name, 'qantity': qantity, 'unit': unit})
+        result = dish_name, structure
+        file.readline()
+        print(result)
+
