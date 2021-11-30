52896d3e467dfb5b778df63da199e36329adfd48
Michail <93787990+michail04@users.noreply.github.com>
2021-12-01 01:42:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -17,28 +17,25 @@ with open(path, encoding="utf8") as file:
             structure.append({'ingredient': name.strip(), 'qantity': int(qantity.strip()), 'measure': measure.strip()})
         cook_book[dish_name] = structure
         file.readline()
-
     print('Cook book=')
     pprint(cook_book)
 
 
-
     def get_shop_list_by_dishes(dishes, person_count):
         person_count = int(input(f'\n Enter the guests count, please: '))
-        dish_order = input('Enter what would u like to meal today: ')
+        # dish_order = input('Enter what would u like to meal today: ')
         menu = cook_book
         list = {}
-        try:
-            for dish in dishes:
-                for ingr in (menu[dish]):
-                    count_dish_name_ingr = dict[ingr['ingredient'], 'measure': ingr['measure'], 'quantity': int(ingr['quantity']) * {person_count}]
-                    if list.get(ingr['ingredient']):
-                        more_ingr = int(ingr['ingredient_name']['quantity']) + int(ingr['ingredient_name']['quantity'])
-                    else:
-                        list.update(count_dish_name_ingr)
+       
+        for dish in dishes:
+            for ingr in (menu[dish]):
+                count_dish_name_ingr = dict[ingr['ingredient'], 'measure': ingr['measure'], 'quantity': int(ingr['quantity']) * {person_count}]
+                if list.get(ingr['ingredient']):
+                    more_ingr = int(ingr['ingredient_name']['quantity']) + int(ingr['ingredient_name']['quantity'])
+                else:
+                    list.update(count_dish_name_ingr)
 
-            print(f"For dishes for {person_count} guests you need:")
-            pprint(list)
+        print(f"For dishes for {person_count} guests you need:")
+        pprint(list)
 
-        except KeyError:
-            print('Sorry, this is not our suggesting art stuff')
+
