28ce14efc8ae6d3a40c47de616ced687d4666226
Michail <93787990+michail04@users.noreply.github.com>
2021-12-01 02:02:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -19,23 +19,23 @@ with open(path, encoding="utf8") as file:
         file.readline()
     print('Cook book=')
     pprint(cook_book)
-
+    # что то я опять сделал не так...
 
     def get_shop_list_by_dishes(dishes, person_count):
         person_count = int(input(f'\n Enter the guests count, please: '))
         # dish_order = input('Enter what would u like to meal today: ')
         menu = cook_book
         list = {}
-       
+
         for dish in dishes:
             for ingr in (menu[dish]):
-                count_dish_name_ingr = dict[ingr['ingredient'], 'measure': ingr['measure'], 'quantity': int(ingr['quantity']) * {person_count}]
+                count_dish_name_ingr = dict[ingr['ingredient'], {'measure': ingr['measure'], 'quantity': int(ingr['quantity']) * person_count}]
                 if list.get(ingr['ingredient']):
-                    more_ingr = int(ingr['ingredient_name']['quantity']) + int(ingr['ingredient_name']['quantity'])
+                    more_ingr = (list['ingredient']['quantity']) + (count_dish_name_ingr['ingredient']['quantity'])
                 else:
                     list.update(count_dish_name_ingr)
 
         print(f"For dishes for {person_count} guests you need:")
         pprint(list)
-
+        # что-то я опять понаписал не так...
 
